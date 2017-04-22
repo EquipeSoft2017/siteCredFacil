93c2e9edd162fb93f119c794ec2a373745650284
Adilson <adilsontp1@gmail.com>
2017-04-22 17:48:39 -0300

    Ajustes na tela de contato e implementação da classe PHPMailer

--- sendemail.php
+++ sendemail.php
@@ -1,21 +1,45 @@
-<?php
-	header('Content-type: application/json');
-	$status = array(
-		'type'=>'success',
-		'message'=>'Thank you for contact us. As early as possible  we will contact you '
-	);
+<?php 
+	
+	//Recebe as variáveis
+	$nome = $_POST["nome"];
+	$email = $_POST["email"];
+	$assunto = $_POST["assunto"];
+	$mensagem = $_POST["mensagem"];
 
-    $name = @trim(stripslashes($_POST['name'])); 
-    $email = @trim(stripslashes($_POST['email'])); 
-    $subject = @trim(stripslashes($_POST['subject'])); 
-    $message = @trim(stripslashes($_POST['message'])); 
+	//Inclui o arquivo class.phpmailer.php
+	include("class.phpmailer.php");
 
-    $email_from = $email;
-    $email_to = 'email@email.com';//replace with your email
+	//Instacia a classe PHPMailer
+	$php_mail = new PHPMailer();
 
-    $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
+	//Define o tipo de conexão e os dados do servidor
+	$php_mail->IsSMTP();
+	$php_mail->Host = "endereço do servidor";
+	$php_mail->SMTPAuth = true; //Caso for usar autenticação SMTP
+	$php_mail->Username = "usuario do servidor";
+	$php_mail->Password = "senha do usuário do servidor";
 
-    $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
+	//Define o emissor
+	$php_mail->From = "email do remetente";
+	$php_mail->FromName = "nome para o remetente";	
 
-    echo json_encode($status);
-    die;
+	//Define o(s) destinatário(s)
+	$php_mail->AddAddress("endereço que vai receber os emails do site");
+
+	//Define que o email será enviado em formato de HTML
+	$php_mail->isHTML(true);
+
+	//Define o assunto e a mensagem do email
+	$php_mail->Subject = $assunto
+	$php_mail->Body = "<strong>Nome: </strong> $nome<br/><br/> <strong>Email : </strong> $email<br/><br/> <strong>Mensagem : </strong> $mensagem";
+
+	//Envia o email
+	$envio = $php_mail->Send();
+
+	//Exibe o resultado
+	if($envio){
+		echo "Email enviado com sucesso!!!";
+	} else {
+		echo "Não foi possível enviar o emil!";
+	}
+?>
